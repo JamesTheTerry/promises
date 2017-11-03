65028ee99a142d444568cfe244689755698b4b90
James Terry <jamesnorthterry@gmail.com>
2017-11-02 19:49:52 -0700

    Completed some advanced work

--- exercises/advanced/collections.js
+++ exercises/advanced/collections.js
@@ -8,9 +8,28 @@
  *    4. Writes the new file to the file located at `writePath`
  */
 
+var fs = require('fs');
+var Promise = require('bluebird');
+var promConHelpers = require('../bare_minimum/promiseConstructor');
+Promise.promisifyAll(fs);
 
 var combineFirstLineOfManyFiles = function(filePaths, writePath) {
- // TODO
+
+  var firstLines = [];
+  for (var i = 0; i < filePaths.length; i++) {
+    firstLines.push(promConHelpers.pluckFirstLineFromFileAsync(filePaths[i]));
+  }
+
+  return Promise.all(firstLines)
+    .then((firstLineArray) => {
+      return firstLineArray.join('\n');
+    })
+    .then((dataToWrite) => {
+      return fs.writeFileAsync(writePath, dataToWrite, 'utf8');
+    })
+    .catch((err) => {
+      console.log('Error: ', err.message);
+    });
 };
 
 // Export these functions so we can unit test them
